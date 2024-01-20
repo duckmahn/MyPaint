655d4512bde885f77e5658ddc359f538fab4eac0
M <21dh113296@st.huflit.edu.vn>
2024-01-20 11:19:40 +0700

    mypaint

---
+++ MyCircle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    internal class MyCircle : Shape
+
+    {
+
+
+        public MyCircle()
+        {
+
+        }
+
+        public MyCircle(Point sPoint, Point ePoint, int borderWidth, Color borderColor) : base (sPoint, ePoint, borderWidth, borderColor)
+        {
+
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(_borderColor, _borderWidth);
+            RectangleF recf = new RectangleF(Location.X, Location.Y, _width, _height);
+            g.DrawEllipse(pen, recf);
+        }
+    }
+}
